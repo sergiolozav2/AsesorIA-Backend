8f61827ba0a225615c47345873d882a5ddc5feb8
Sergio Loza <sergiolozav@gmail.com>
2024-05-02 17:55:49 -0400

    test: Removido temporalmente

--- vitest.config.js
+++
@@ -1,9 +0,0 @@
-import tsconfigPaths from 'vite-tsconfig-paths'; // only if you are using custom tsconfig paths
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  test: {
-    setupFiles: ['./src/__test__/setup.ts'],
-  },
-  plugins: [tsconfigPaths()], // only if you are using custom tsconfig paths
-});
